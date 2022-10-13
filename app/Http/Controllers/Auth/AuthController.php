7a70cf4fd8b435b3bea059f3843e78d89213de89
razvan-bara <rzvbara@gmail.com>
2022-10-13 17:46:47 +0300

    created the login callback for the endpoint

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
@@ -32,4 +35,37 @@ class AuthController extends Controller
             ]
         ,Response::HTTP_CREATED);
     }
+
+    public function login(LoginRequest $request){
+        
+        $credentials = $request->validated();
+ 
+        if (Auth::attempt($credentials)) {
+
+            $token = Str::random(32);
+            $user = Auth::user();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login cu succes',
+                'data'      => [
+                    'user' => [ 
+                        'first_name' => $user->first_name,
+                        'last_name' => $user->last_name,
+                        'email' => $user->email,
+                    ],
+                    'token' => $token,
+                ]
+            ],Response::HTTP_OK);
+
+        } 
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Login esuat, verificati credentialele',
+            'data'      => []
+        ],Response::HTTP_UNAUTHORIZED);
+
+    }
+
 }
